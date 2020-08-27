e907194e336563821c1c9ee89a6683e327479e93
rsrizwan77 <rsrizwan77@gmail.com>
2020-08-27 06:42:00 +0000

    Adding program largest of array

---
+++ Largestnumber_Array/src/com/stackroute/LargestElement.java
@@ -0,0 +1,37 @@
+package com.stackroute;
+
+import java.util.Scanner;
+
+public class LargestElement {
+/*Class to find the largest element in the array*/
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the elements of the array");
+        int len = s.nextInt();
+        int num[]=new int[len];
+        for(int i =0; i<len; i++)
+        {
+            System.out.println("Enter the number "+(i+1));
+            num[i]=s.nextInt();
+        }
+
+        System.out.println(" The largest element is:"+findlarge(num));
+
+    }
+
+    /*Method to find the largest number*/
+    public static int findlarge(int num[])
+    {
+        int len =  num.length;
+        int max = 0;
+        for(int i=0;i<len;i++)
+        {
+/*Check if max value is greater than current value of m if then replace max with number*/
+            if(max<num[i])
+            {
+                max= num[i];
+            }
+        }
+        return max;
+    }
+}
